c2b5b2373de6d0916f4cc2268f308ed336a6cbec
ValentinAndreev <valentinandreev80@gmail.com>
2017-03-18 21:45:06 +0300

    Added edit from show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def sanitz(post)
-    sanitize post.text, tags: %w(a b i strong em p h1 h2 h3 h4 h5 h6 br hr ul li img pre), attributes: %w(href name src type value width height style)
+    sanitize post.text, tags: %w(a b i strong em p h1 h2 h3 h4 h5 h6 br hr ul li img pre blockquote), attributes: %w(href name src type value width height style)
   end
 
   def commts(post)
